c272dbb98d726862a2d73943ca1ccb1d003901ca
Amit <ay.puzzler@gmail.com>
2023-07-18 11:27:42 +0530

    Create 04-use-function-types-and-lambda-expressions-in-kotlin.kt

---
+++ 04-use-function-types-and-lambda-expressions-in-kotlin.kt
@@ -0,0 +1,54 @@
+/**
+ * Kotlin Fundamentals
+ * from Android Team
+ * 
+ * 
+ * Use function types and lambda expressions in Kotlin
+ */
+
+// #########################################################################################################################
+
+
+val trick = {
+    println("no treats")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick) {
+        return trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+
+fun main() {
+//     val coins: (Int) -> String = { 
+//         "$it quarters"
+//     }
+    
+//     val cupcake: (Int) -> String = { 
+//         "Have a cupcake!"
+//     }
+    
+//     val treatFunction = trickOrTreat(false, coins)
+//     val treatFunction = trickOrTreat(false, { "$it quarters" })
+    val treatFunction = trickOrTreat(false) { "$it quarters" }
+    val trickFunction = trickOrTreat(true, null)
+//     treatFunction()
+//     
+
+	repeat(4) {
+        treatFunction()
+    }
+    
+    trickFunction()
+}
+
+// #########################################################################################################################
